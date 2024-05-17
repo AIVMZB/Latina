7c0647e11abe1d3d70b2b74f89a9145ed4452523
Max Voloshchuk <68013193+Zarathustra4@users.noreply.github.com>
2024-05-17 14:39:13 +0300

    YOLO-OBB for line detection (#14)
    * yolo-oob is trained for lines
    
    * all images from test set are tested

--- src/word_detection.py
+++ src/word_detection.py
@@ -13,20 +13,25 @@ print(f"[DEVICE] - {dev}")
 TORCH_DEVICE = torch.device(dev)
 YOLO_DEVICE = [0] if dev == "cuda:0" else "cpu"   
 
-IMG_SIZE = 900
+IMG_SIZE = 1024
 YOLO_MOLDEL = os.path.join("models", "yolov8n.pt")
-BEST_TRAINED_MODEL = os.path.join("models", "best.pt")
+WORD_DETECT_BEST_MODEL = os.path.join("models", "word_detect_best.pt")
+LINES_OBB_BEST = os.path.join("models", "lines_obb_best.pt")
 
 
-def train_detection_model(model_path: str = YOLO_MOLDEL, epochs: int = 50) -> None:
+def train_detection_model(model_path: str = YOLO_MOLDEL, 
+                          data_file: str = "words_data.yaml",
+                          epochs: int = 50) -> None:
     """Trains a model. The results of training will be stored at run/detect directory"""
 
     model = YOLO(model_path).to(TORCH_DEVICE)
-    model.train(data="data.yaml", epochs=epochs, batch=1, imgsz=IMG_SIZE, device=YOLO_DEVICE, workers=2)
+    model.train(data=data_file, epochs=epochs, batch=1, imgsz=IMG_SIZE, device=YOLO_DEVICE, workers=1,
+                hsv_h=0.0, hsv_s=0.0, hsv_v=0.0, translate=0.1, scale=0.1, fliplr=0.0, mosaic=0.0, erasing=0.0,
+                crop_fraction=0.1)
 
 
-def inference(images_dir: str, 
-              model_path: str = BEST_TRAINED_MODEL, 
+def inference(images_dir: str,
+              model_path: str = WORD_DETECT_BEST_MODEL, 
               min_confidence: float = 0.28,
               show=False) -> None:
     """Inferences a model. The results will be saved at prediction directory"""
@@ -37,20 +42,28 @@ def inference(images_dir: str,
 
     images = list(map(lambda name: os.path.join(images_dir, name), image_names))
 
-    results = model(images, conf=min_confidence)
+    results = model.predict(images, conf=min_confidence)
 
     if not os.path.exists("../predictions"):
         os.mkdir("../predictions")
 
     for i, result in enumerate(results):
-        result.plot(labels=False, probs=False, show=show, save=True, line_width=2,
-                    filename=os.path.join("predictions", image_names[i]))
+        result.plot(labels=True, probs=False, show=show, save=True, line_width=2,
+                    filename=os.path.join("../predictions", image_names[i]))
 
 
 if __name__ == "__main__":
-    # Uncomment the line below to train model for 5 epochs
-    # train_detection_model(epochs=5)
+    # Uncomment the line below to train to detect lines oriented bboxes
+    # train_detection_model(epochs=5, data_file="lines_obb_data.yaml", model_path="yolov8n-obb.pt")
 
-    # Uncomment the line below to test model on images
-    # inference("../images", BEST_TRAINED_MODEL)
+    
+    # Uncomment the line below to train to detect words bboxes
+    # train_detection_model(epochs=5, data_file="words_data.yaml", model_path="yolov8n.pt")
+
+
+    # Uncomment the line below to test model on detecting words
+    # inference("../images", WORD_DETECT_BEST_MODEL, min_confidence=0.3)
+
+    # Uncomment the line below to test model on detecting lines
+    inference("../datasets/lines-obb/valid/images", LINES_OBB_BEST, min_confidence=0.1, show=True)
     ...
