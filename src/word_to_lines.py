1886048ea7eaa95971eb8c067daa171cb6412828
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2024-06-13 21:43:22 +0300

    Creating dataset of lines

--- src/word_to_lines.py
+++ src/word_to_lines.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import shapes_util as su
 from skimage.draw import polygon2mask
+import os
 
 
 # TODO: Implement transforming words coordinates to line space according to the belonging
@@ -98,29 +99,111 @@ def words_to_line_space(words: list[Bbox], line: Obb) -> list[Bbox]:
 
     return line_words
 
+def write_bboxes_to_file(bboxes: list[Bbox], filename: str, class_name: str) -> None:
+    with open(filename, 'w') as file:
+        for bbox in bboxes:
+            line = class_name + " " + " ".join(map(str, bbox)) + "\n"
+            file.write(line)
+
+
+def create_lines_dataset_from_image(image_name: str, image_dir: str, 
+                                    lines: list[Obb], words: list[Bbox], output_dir: str, 
+                                    word_class: str = '0',
+                                    white_bg: bool = False) -> None:
+    if not os.path.exists(os.path.join(image_dir, image_name)):
+        raise ValueError(f"There is no such an image {os.path.join(image_dir, image_name)}")
+    
+    image = cv2.imread(os.path.join(image_dir, image_name))
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    
+    line_to_words = su.map_words_to_lines(words, lines, image)
+    for line_idx in line_to_words:
+        line = lines[line_idx]
+
+        line_image = crop_line_from_image(image, line, white_bg=white_bg)
+        line_image_path = os.path.join(output_dir, f"{image_name[:-4]}_{line_idx}.jpg")
+
+        try:
+            cv2.imwrite(line_image_path, line_image)
+        except:
+            print(f"\t[WARNING] Failed to save line image with index {line_idx}")
+        
+        line_words = [words[idx] for idx in line_to_words[line_idx]]
+        line_words = words_to_line_space(line_words, line)
+
+        write_bboxes_to_file(line_words, os.path.join(output_dir, f"{image_name}_{line_idx}.txt"), word_class)
+    
+
+def find_label_file_in_dirs(dirs: list[str], image_name: str) -> str:
+    for dir in dirs:
+        for file in os.listdir(dir):
+            if file.startswith(image_name[:-12]) and file.endswith(".txt"):
+                return os.path.join(dir, file)
+
+def create_dataset_of_lines(words_dataset_path: str = "..\datasets\words",
+                            lines_dataset_path: str = "..\datasets\lines-obb",
+                            output_dataset_path: str = "..\datasets\words-in-lines"):
+    word_train_dir = os.path.join(words_dataset_path, "train")
+    word_val_dir = os.path.join(words_dataset_path, "validation")
+    
+    lines_train_dir = os.path.join(lines_dataset_path, "train", "labels")
+    lines_test_dir = os.path.join(lines_dataset_path, "test", "labels")
+    lines_val_dir = os.path.join(lines_dataset_path, "valid", "labels")
+    
+    print("------------------ Train ------------------")
+    for word_image_name in os.listdir(word_train_dir)[1:]:
+        if not word_image_name.endswith(".jpg"):
+            continue
+
+        word_label_file = os.path.join(word_train_dir, word_image_name[:-4] + ".txt")
+        line_label_file = find_label_file_in_dirs([lines_train_dir, lines_test_dir, lines_val_dir], word_image_name)
+        if line_label_file is None:
+            print(f"[WARNING] {word_image_name} is not found in lines dataset")
+            continue
+        
+        lines = su.read_shapes(line_label_file, transform_func=su.to_obb)
+        words = su.read_shapes(word_label_file, transform_func=su.to_bbox)
+
+        print(f"[INFO] Processing {word_image_name}")
+        try:
+            create_lines_dataset_from_image(word_image_name, word_train_dir, lines, words, output_dataset_path, white_bg=False)
+        except Exception as e:
+            print(f"[ERROR] {word_image_name} failed")
+            print(e)
+
+    ######################################
+    print("------------------ Validation ------------------")
+    for word_image_name in os.listdir(word_val_dir):
+        if not word_image_name.endswith(".jpg"):
+            continue
+
+        word_label_file = os.path.join(word_val_dir, word_image_name[:-4] + ".txt")
+        line_label_file = find_label_file_in_dirs([lines_train_dir, lines_test_dir, lines_val_dir], word_image_name)
+        if line_label_file is None:
+            print(f"[WARNING] {word_image_name} is not found in lines dataset")
+            continue
+        
+        lines = su.read_shapes(line_label_file, transform_func=su.to_obb)
+        words = su.read_shapes(word_label_file, transform_func=su.to_bbox)
+
+        print(f"[INFO] Processing {word_image_name}")
+        try:
+            create_lines_dataset_from_image(word_image_name, word_val_dir, lines, words, output_dataset_path, white_bg=False)
+        except Exception as e:
+            print(f"[ERROR] {word_image_name} failed")
+            print(e)
 
 if __name__ == "__main__":
     # TODO: Create datased with image for each
     image = cv2.imread(
-        "..\datasets\lines-obb\\train\images\AUR_996_V_28-101-text-_jpg.rf.51ea5ad9e77ebc31d1c6c1b66cf5adb1.jpg")
+        "..\datasets\words\\validation\AUR_889_X_5(15)-101 (text).jpg")
     lines: list[Obb] = su.read_shapes(
-        "..\datasets\lines-obb\\train\labels\AUR_996_V_28-101-text-_jpg.rf.51ea5ad9e77ebc31d1c6c1b66cf5adb1.txt",
+        "..\datasets\lines-obb\\test\labels\AUR_889_X_5-15-101-text-_jpg.rf.e2800ba0157f1988cbd011f68c072622.txt",
         transform_func=su.to_obb)
-    words = su.read_shapes("..\datasets\words\\train\AUR_996_V_28-101 (text).txt", transform_func=su.to_bbox)
+    words = su.read_shapes("..\datasets\words\\validation\AUR_889_X_5(15)-101 (text).txt", transform_func=su.to_bbox)
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    line_to_words = su.map_words_to_lines(words, lines, image)
-
-    for line_idx in line_to_words:
-        line = lines[line_idx]        
-        im_line = crop_line_from_image(image, line, white_bg=True)
-
-        line_words = [words[index] for index in line_to_words[line_idx]]
-        line_words = words_to_line_space(line_words, line)
-        for word in line_words:
-            im_line = su.plot_obb_on_image(im_line, su.to_obb(word))
-
-        plt.imshow(im_line)
-        plt.show()
-
+    image_name = "AUR_889_X_5(15)-101 (text).jpg"
+    
+    create_lines_dataset_from_image(image_name, "..\datasets\words\\validation", lines, words, "..\datasets\words-in-lines", white_bg=False)
