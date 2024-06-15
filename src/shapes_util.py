603a45fcff48ea945e1ff5031b660c748053429c
Max Voloshchuk <68013193+Zarathustra4@users.noreply.github.com>
2024-06-15 12:47:42 +0300

    Creating datasets of line images (#19)
    * starting processing bboxes
    
    * new functions
    
    * words intersection done
    
    * Docstrings and map function
    
    * some function are renamed | word_to_lines.py is created
    
    * word_to_lines.py is created
    
    * transforming words to line's space
    
    * words to line coordinates
    
    * Creating dataset of lines
    
    * splitting lines dataset
    
    * Lines dataset is created
    
    * fix
    
    * Models combination

---
+++ src/shapes_util.py
@@ -0,0 +1,229 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon
+from typing import NamedTuple, Union, Sequence
+from prettyprinter import pprint
+
+
+class Bbox(NamedTuple):
+    cx: float
+    cy: float
+    w: float
+    h: float
+
+
+class Obb(NamedTuple):
+    x1: float
+    y1: float
+    x2: float
+    y2: float
+    x3: float
+    y3: float
+    x4: float
+    y4: float
+
+
+def to_bbox(bbox_list: Sequence) -> Bbox:
+    """
+    Creates a Bbox object from a sequence of values.
+
+    Args:
+        bbox_list (Sequence): A sequence of values representing the coordinates of a bounding box. The sequence should have exactly four values: [x, y, width, height].
+
+    Returns:
+        Bbox: A Bbox object representing the bounding box with the given coordinates.
+    """
+
+    return Bbox(*bbox_list)
+
+
+def to_obb(shape: Union[Bbox, Sequence]) -> Obb:
+    """
+    Convert a shape to an Obb (Oriented Bounding Box) object.
+
+    Args:
+        shape (Union[Bbox, Sequence]): The shape to be converted. It can be either a Bbox object or a sequence of values.
+
+    Returns:
+        Obb: The Obb object representing the converted shape.
+
+    Raises:
+        TypeError: If the shape is neither a Bbox object nor a sequence of values.
+
+    """
+    if isinstance(shape, Bbox):
+        return bbox_to_obb(shape)
+    elif isinstance(shape, Sequence):
+        return Obb(*shape)
+
+
+def read_shapes(filename: str, class_num: str = "0", 
+                transform_func: callable = to_bbox) -> Union[list[Bbox], list[Obb]]:
+    """
+    Reads shapes from a file and transforms them using a given function.
+
+    Args:
+        filename (str): The name of the file to read.
+        class_num (str, optional): The class number to filter the shapes. Defaults to "0".
+        transform_func (callable, optional): The function to transform the shapes. Defaults to to_bbox.
+
+    Returns:
+        Union[list[Bbox], list[Obb]]: A list of readed shapes.
+    """
+    shapes = []
+    with open(filename, 'r') as file:
+        for line in file:
+            values = line.split(" ")
+            if values[0] != class_num:
+                continue
+            shapes.append(
+                transform_func(list(map(float, values[1:])))
+            )
+    
+    return shapes
+
+
+def bbox_to_obb(bbox: Union[list, Bbox]) -> Obb:
+    cx, cy = bbox[0], bbox[1]
+    W, H = bbox[2], bbox[3]
+    w, h = W / 2, H / 2
+
+    return to_obb([cx - w, cy + h, cx + w, cy + h, cx + w, cy - h, cx - w, cy - h])
+
+
+def obb_to_polygon(obb: Union[list, Obb]) -> Polygon:
+    points = []
+    for i in range(0, len(obb), 2):
+        points.append((obb[i], obb[i + 1]))
+
+    return Polygon(points)
+
+
+def intersection_area(shape_1: Obb, shape_2: Obb) -> list:
+    pol_1 = obb_to_polygon(shape_1)
+    pol_2 = obb_to_polygon(shape_2)
+
+    return pol_1.intersection(pol_2).area
+
+
+def obb_to_image_coords(width: int, height: int, obb: Obb) -> Obb:
+    image_obb = []
+    for i in range(len(obb)):
+        if i % 2 == 0:
+            image_obb.append(int(obb[i] * width))
+        else:
+            image_obb.append(int(obb[i] * height))
+
+    return to_obb(image_obb)
+
+
+def find_line_for_word(word_obb: Obb, line_bboxes: list[Obb]) -> Obb:
+    """Counts intersection of lines with the specified word and picks a line with the highest intersection area"""
+
+    best_intersection = 0
+    best_line_index = None
+    for i, line_bbox in enumerate(line_bboxes):
+        intersection = intersection_area(word_obb, line_bbox)
+        if intersection > best_intersection:
+            best_intersection = intersection
+            best_line_index = i
+            
+    return best_line_index
+
+
+def find_words_in_line(line_obb: Obb, word_bboxes: list[Bbox], 
+                       width: int, height: int, 
+                       thresh: float = 0.5) -> list:
+    """
+    Finds the indexes of words that intersect with a given line.
+
+    Args:
+        line_obb (Obb): The oriented bounding box of the line.
+        word_bboxes (list[Bbox]): The bounding boxes of the words.
+        width (int): The width of the image.
+        height (int): The height of the image.
+        thresh (float, optional): The threshold for intersection area. Defaults to 0.5.
+
+    Returns:
+        list: The indexes of the intersected words.
+    """
+    
+    intersected_words_indexes = []
+    line_obb = obb_to_image_coords(width, height, line_obb)
+    for i, word_bbox in enumerate(word_bboxes):
+        word_obb = bbox_to_obb(word_bbox)
+        word_obb = obb_to_image_coords(width, height, word_obb)
+        
+        if intersection_area(line_obb, word_obb) / obb_to_polygon(word_obb).area > thresh:
+            intersected_words_indexes.append(i)
+
+    return intersected_words_indexes
+
+
+def plot_obb_on_image(image: np.ndarray, obb: Obb, color: tuple = (0, 0, 255)) -> np.ndarray:
+    im_obb = np.zeros((4, 2), dtype=np.int32)
+    width = image.shape[0]
+    height = image.shape[1]
+
+    for i in range(0, len(obb), 2):
+        im_obb[i // 2][0] = round(obb[i] * height) if obb[i] > 0 else 0
+        im_obb[i // 2][1] = round(obb[i + 1] * width) if obb[i + 1] > 0 else 0
+    
+    im_obb = im_obb.reshape((-1, 1, 2))
+
+    return cv2.polylines(image, [im_obb], True, color, 5)
+
+
+def line_bbox_on_image(image: np.ndarray, bbox: Bbox, color: tuple = (0, 0, 255)) -> np.ndarray:
+    obb = bbox_to_obb(bbox)
+
+    return plot_obb_on_image(image, obb, color)
+
+
+def plot_word_and_line(image: np.ndarray, word: Bbox, lines: list[Obb]) -> np.ndarray:
+    line = find_line_for_word(word, lines)
+
+    image = plot_obb_on_image(image, line)
+    return plot_obb_on_image(image, word, (255, 0, 0))
+
+
+def index_in_map(table: dict, index: int) -> bool:
+    for values in table.values():
+        if index in values:
+            return True
+
+    return False
+
+
+def map_words_to_lines(words: list[Bbox], lines: list[Obb], image: np.ndarray) -> dict:
+    line_to_words = {}
+    count = 0
+    for i, line in enumerate(lines):
+        line_to_words[i] = find_words_in_line(line, words, image.shape[1], image.shape[0])
+        count += len(line_to_words[i])
+    
+    if count == len(words):
+        return line_to_words
+    
+    for i, word in enumerate(words):
+        if index_in_map(line_to_words, i):
+            continue
+        
+        line_index = find_line_for_word(to_obb(word), lines)
+
+        line_to_words[line_index].append(i)
+
+    return line_to_words
+
+
+if __name__ == "__main__":
+    lines = read_shapes("..\datasets\lines-obb\\test\labels\AUR_945_VI_4-101-text-_jpg.rf.f05201b45f7215a2eb52c9750eed34e6.txt",
+                        transform_func=to_obb)
+    words = read_shapes("..\datasets\words\\train\AUR_945_VI_4-101 (text).txt",
+                        transform_func=to_bbox)
+
+    image = cv2.imread("../datasets/lines-obb/test/images/AUR_945_VI_4-101-text-_jpg.rf.f05201b45f7215a2eb52c9750eed34e6.jpg")
+
+    line_to_words = map_words_to_lines(words, lines, image)
+    pprint(line_to_words)
---
+++ src/word_to_lines.py
@@ -0,0 +1,238 @@
+from shapes_util import Bbox, Obb
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import shapes_util as su
+from skimage.draw import polygon2mask
+import os
+import random
+import shutil
+
+
+# TODO: Implement transforming words coordinates to line space according to the belonging
+
+def obb_to_nparray(obb: Obb) -> np.ndarray:
+    points = []
+    for i in range(0, len(obb), 2):
+        points.append([obb[i], obb[i + 1]])
+
+    return np.array(points)
+
+
+def obb_to_bbox(line: Obb) -> Bbox:
+    points = obb_to_nparray(line)
+    x, y, w, h = cv2.boundingRect(points)
+    x = (x + w) / 2
+    y = (y + h) / 2
+    return Bbox(x, y, w, h)
+
+
+def obb_to_rec(obb: Obb) -> list:
+    """Returns x, y, w, h"""
+    x = min(obb.x1, obb.x2, obb.x3, obb.x4)
+    y = min(obb.y1, obb.y2, obb.y3, obb.y4)
+    w = max(obb.x1, obb.x2, obb.x3, obb.x4) - x
+    h = max(obb.y1, obb.y2, obb.y3, obb.y4) - y
+
+    return [x, y, w, h]
+
+
+def crop_line_from_image(image: np.ndarray, line: Obb, white_bg: bool = False) -> np.ndarray:
+    line = su.obb_to_image_coords(image.shape[1], image.shape[0], line)
+    np_line = obb_to_nparray(line)
+    for i in range(np_line.shape[0]):
+        np_line[i][0], np_line[i][1] = np_line[i][1], np_line[i][0]
+
+    line_mask = polygon2mask(image.shape[:2], np_line)
+
+    line_mask = np.stack([line_mask, line_mask, line_mask], axis=-1)
+
+    masked_image = image * line_mask
+    if white_bg:
+        masked_image = np.vectorize(lambda x: 255 if x == 0 else x)(masked_image)
+
+    x, y, w, h = obb_to_rec(line)
+
+    croped = masked_image[y:y+h, x:x+w].copy()
+
+    return croped
+
+
+def bbox_to_line_space(bbox: Bbox, line: list) -> Bbox:
+    """
+    Transforms the bounding box coordinates to line space according to the provided line.
+
+    Args:
+        bbox (Bbox): The bounding box coordinates.
+        line (list): The line in form of rectangle (x, y, w, h).
+
+    Returns:
+        Bbox: The transformed bounding box in line space.
+    """
+    bbox_center = np.array([bbox.cx, bbox.cy])
+    bbox_size = np.array([bbox.w, bbox.h])
+    line_point = np.array([line[0], line[1]])
+    line_size = np.array([line[2], line[3]])
+
+    line_bbox_center = (bbox_center - line_point) / line_size
+    line_bbox_size = bbox_size / line_size
+
+    bbox = Bbox(*line_bbox_center, *line_bbox_size)
+
+    # if bbox.cx + bbox.w / 2 > 1:
+    #     bbox = Bbox(bbox.cx, bbox.cy, 2 * (1 - bbox.cx), bbox.h)
+    # if bbox.cx - bbox.w / 2 < 0:
+    #     bbox = Bbox(bbox.cx, bbox.cy, 2 * bbox.cx, bbox.h)
+
+    # if bbox.cy + bbox.h / 2 > 1:
+    #     bbox = Bbox(bbox.cx, bbox.cy, bbox.w, 2 * (1 - bbox.cy))
+    # if bbox.cy - bbox.h / 2 < 0:
+    #     bbox = Bbox(bbox.cx, bbox.cy, bbox.w, 2 * bbox.cy)
+
+    return bbox
+
+
+def words_to_line_space(words: list[Bbox], line: Obb) -> list[Bbox]:
+    line_words = []
+    line_rec = obb_to_rec(line)
+    for word in words:
+        word_in_line = bbox_to_line_space(word, line_rec)
+        line_words.append(word_in_line)
+
+    return line_words
+
+
+def write_bboxes_to_file(bboxes: list[Bbox], filename: str, class_name: str) -> None:
+    with open(filename, 'w') as file:
+        for bbox in bboxes:
+            line = class_name + " " + " ".join(map(str, bbox)) + "\n"
+            file.write(line)
+
+
+def create_lines_dataset_from_image(image_name: str, image_dir: str, 
+                                    lines: list[Obb], words: list[Bbox], output_dir: str, 
+                                    word_class: str = '0',
+                                    white_bg: bool = False) -> None:
+    if not os.path.exists(os.path.join(image_dir, image_name)):
+        raise ValueError(f"There is no such an image {os.path.join(image_dir, image_name)}")
+    
+    image = cv2.imread(os.path.join(image_dir, image_name))
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    
+    line_to_words = su.map_words_to_lines(words, lines, image)
+    for line_idx in line_to_words:
+        line = lines[line_idx]
+
+        line_image = crop_line_from_image(image, line, white_bg=white_bg)
+        line_image_path = os.path.join(output_dir, f"{image_name[:-4]}_{line_idx}.jpg")
+
+        try:
+            cv2.imwrite(line_image_path, line_image)
+        except:
+            print(f"\t[WARNING] Failed to save line image with index {line_idx}")
+        
+        line_words = [words[idx] for idx in line_to_words[line_idx]]
+        line_words = words_to_line_space(line_words, line)
+
+        write_bboxes_to_file(line_words, os.path.join(output_dir, f"{image_name[:-4]}_{line_idx}.txt"), word_class)
+    
+
+def find_label_file_in_dirs(dirs: list[str], image_name: str) -> str:
+    for dir in dirs:
+        for file in os.listdir(dir):
+            if file.startswith(image_name[:-12]) and file.endswith(".txt"):
+                return os.path.join(dir, file)
+
+
+def create_dataset_of_lines(words_dataset_path: str = "..\datasets\words",
+                            lines_dataset_path: str = "..\datasets\lines-obb",
+                            output_dataset_path: str = "..\datasets\words-in-lines"):
+    word_train_dir = os.path.join(words_dataset_path, "train")
+    word_val_dir = os.path.join(words_dataset_path, "validation")
+    
+    lines_train_dir = os.path.join(lines_dataset_path, "train", "labels")
+    lines_test_dir = os.path.join(lines_dataset_path, "test", "labels")
+    lines_val_dir = os.path.join(lines_dataset_path, "valid", "labels")
+    
+    print("------------------ Train ------------------")
+    for word_image_name in os.listdir(word_train_dir)[1:]:
+        if not word_image_name.endswith(".jpg"):
+            continue
+
+        word_label_file = os.path.join(word_train_dir, word_image_name[:-4] + ".txt")
+        line_label_file = find_label_file_in_dirs([lines_train_dir, lines_test_dir, lines_val_dir], word_image_name)
+        if line_label_file is None:
+            print(f"[WARNING] {word_image_name} is not found in lines dataset")
+            continue
+        
+        lines = su.read_shapes(line_label_file, transform_func=su.to_obb)
+        words = su.read_shapes(word_label_file, transform_func=su.to_bbox)
+
+        print(f"[INFO] Processing {word_image_name}")
+        try:
+            create_lines_dataset_from_image(word_image_name, word_train_dir, lines, words, output_dataset_path, white_bg=False)
+        except Exception as e:
+            print(f"[ERROR] {word_image_name} failed")
+            print(e)
+
+    ######################################
+    print("------------------ Validation ------------------")
+    for word_image_name in os.listdir(word_val_dir):
+        if not word_image_name.endswith(".jpg"):
+            continue
+
+        word_label_file = os.path.join(word_val_dir, word_image_name[:-4] + ".txt")
+        line_label_file = find_label_file_in_dirs([lines_train_dir, lines_test_dir, lines_val_dir], word_image_name)
+        if line_label_file is None:
+            print(f"[WARNING] {word_image_name} is not found in lines dataset")
+            continue
+        
+        lines = su.read_shapes(line_label_file, transform_func=su.to_obb)
+        words = su.read_shapes(word_label_file, transform_func=su.to_bbox)
+
+        print(f"[INFO] Processing {word_image_name}")
+        try:
+            create_lines_dataset_from_image(word_image_name, word_val_dir, lines, words, output_dataset_path, white_bg=False)
+        except Exception as e:
+            print(f"[ERROR] {word_image_name} failed")
+            print(e)
+
+
+def split_dataset(dataset_path: str, output_path: str):
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    
+    if not os.path.exists(os.path.join(output_path, "train")):
+        os.mkdir(os.path.join(output_path, "train"))
+    
+    if not os.path.exists(os.path.join(output_path, "validation")):
+        os.mkdir(os.path.join(output_path, "validation"))
+    
+    for file in os.listdir(dataset_path):
+        if file.endswith(".jpg"):
+            if random.random() < 0.8:
+                shutil.copy(os.path.join(dataset_path, file), os.path.join(output_path, "train", file))
+                shutil.copy(os.path.join(dataset_path, file[:-4] + ".txt"), os.path.join(output_path, "train", file[:-4] + ".txt"))
+            else:
+                shutil.copy(os.path.join(dataset_path, file), os.path.join(output_path, "validation", file))
+                shutil.copy(os.path.join(dataset_path, file[:-4] + ".txt"), os.path.join(output_path, "validation", file[:-4] + ".txt"))
+
+
+if __name__ == "__main__":
+    # TODO: split to train and validation
+
+    # Creation of lines dataset
+    create_dataset_of_lines()
+    image_name = "AUR_889_X_5(15)-101 (text).jpg"
+    image_dir = "..\\datasets\\words\\validation"
+    lines = su.read_shapes("..\datasets\lines-obb\\test\labels\AUR_889_X_5-15-101-text-_jpg.rf.e2800ba0157f1988cbd011f68c072622.txt",
+                            transform_func=su.to_obb)
+    words = su.read_shapes("..\datasets\words\\validation\AUR_889_X_5(15)-101 (text).txt", 
+                           transform_func=su.to_bbox)
+    create_lines_dataset_from_image(image_name, image_dir, lines, words, "..\datasets\words-in-lines", white_bg=False)
+
+    # Splitting it to train and validation
+    split_dataset("..\datasets\words-in-lines", "..\datasets\words-in-lines-splitted")
+    ...
